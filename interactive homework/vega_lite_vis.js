0881ff6c65a6882e696313d021963e9971e2693b
choubao <89794565+cyeo0023@users.noreply.github.com>
2022-10-09 06:11:15 +0800

    vegalite javascript is uploaded

---
+++ interactive homework/vega_lite_vis.js
@@ -0,0 +1,6 @@
+var spec2 = "week10Homework.json";
+var spec3 = "week9Homework.json";
+vegaEmbed('#bubble_plot', spec2).then(function(result) {
+}).catch(console.error);
+vegaEmbed('#map', spec3).then(function(result) {
+}).catch(console.error);
